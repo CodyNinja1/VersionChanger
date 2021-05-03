92338487b51398dbf8413cf6361f7a1b4778fded
codyninja1 <82186696+CodyNinja1@users.noreply.github.com>
2021-05-03 16:31:12 +0300

    added the ablity to change the author and the package

--- versionUpdate.py
+++ versionUpdate.py
@@ -14,6 +14,8 @@ configData = json.load(configFile)
 
 encodingType = configData["encoding"]
 version = configData["version"]
+package = configData["package"]
+author = configData["author"]
 iterations = configData["iterations"]
 ignoredFolders = configData["ignore"]
 extentions = configData["extentions"]
@@ -22,8 +24,11 @@ logFilePlace = configData["logFile"]
 # Variables
 
 lineReplacementCounter = 0
+switchCounter = 0
 
 versionText = " * @version "
+packageText = " * @package "
+authorText = " * @author "
 firstGoodLines = ""
 secondGoodLines = ""
 
@@ -31,6 +36,7 @@ switch = False
 breakSwitch = False
 
 files = []
+toChange = [[version, versionText], [package, packageText], [author, authorText]]
 
 # Log File
 try:
@@ -41,6 +47,7 @@ except FileExistsError:
     logFile = open(logFilePlace, "a")
 
 logFile.write("[" + str(datetime.now()) + "]: New Instance created.\n")
+logFile.close()
 
 for iteration in range(iterations):
     for extention in extentions:
@@ -50,36 +57,45 @@ for iteration in range(iterations):
 
 
 # File Reading and Editing
-
-for file in files:
-    for ignoredFolder in ignoredFolders:
-        if ignoredFolder in file:
-            logFile.write("[" + str(datetime.now()) + f"]: Directory {ignoredFolder} has been skipped.\n")
-            breakSwitch = True
-    if breakSwitch:
+for change in toChange:
+    if change[0] == "":
         continue
-    oldFile = open(file, encoding=encodingType)
-    lines = oldFile.readlines()
-    oldFile.close()
-    os.remove(file)
-    otherTemp = open(file, "x", encoding=encodingType)
-    otherTemp.close()
-    temp = open(file, "r+", encoding=encodingType)
-    for line in lines:
-        if versionText not in line:
-            if not switch:
-                firstGoodLines = firstGoodLines + line
+    logFile = open(logFilePlace, "a")
+    for file in files:
+        for ignoredFolder in ignoredFolders:
+            if ignoredFolder in file:
+                logFile.write("[" + str(datetime.now()) + f"]: Directory {ignoredFolder} has been skipped because of {file}.\n")
+                breakSwitch = True
+        if breakSwitch:
+            continue
+        oldFile = open(file, encoding=encodingType)
+        lines = oldFile.readlines()
+        oldFile.close()
+        os.remove(file)
+        otherTemp = open(file, "x", encoding=encodingType)
+        otherTemp.close()
+        temp = open(file, "r+", encoding=encodingType)
+        for line in lines:
+            if change[1] + change[0] == line:
+                break
+            if change[1] not in line:
+                if not switch:
+                    firstGoodLines = firstGoodLines + line
+                else:
+                    secondGoodLines = secondGoodLines + line
             else:
-                secondGoodLines = secondGoodLines + line
-        else:
-            switch = True
-            
-    replacementLine = versionText + version
-    final = "" + firstGoodLines + replacementLine + "\n" + secondGoodLines
-    temp.write(final)
-    temp.close()
+                switch = True
+                
+        replacementLine = change[1] + change[0]
+        final = "" + firstGoodLines + replacementLine + "\n" + secondGoodLines
+        temp.write(final)
+        temp.close()
+        firstGoodLines, secondGoodLines, replacementLine = ("", "", "")
+        switch = False
+        breakSwitch = False
+        logFile.write("[" + str(datetime.now()) + f"]: File {file} has been edited.\n")
+    file = ""
     firstGoodLines, secondGoodLines, replacementLine = ("", "", "")
     switch = False
     breakSwitch = False
-    logFile.write("[" + str(datetime.now()) + f"]: File {file} has been edited.\n")
-logFile.close()
+    logFile.close()
